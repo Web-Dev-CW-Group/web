94827598bc3671aea2b3d234ac5cd550255b4a62
Thejan Vithanage <thejanvithanage@outlook.com>
2023-03-13 19:19:10 +0530

    update

---
+++ scripts/quiz_script.js
@@ -0,0 +1,7 @@
+const startBtn = document.getElementById('start-btn')
+const nextBtn = document.getElementById('next-btn')
+const quizContanerElement = document.getElementById('quiz-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
+
+let shuffledQuestions, currentQuestionIndex
